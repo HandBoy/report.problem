10a0693c1db284b0df367df6319ab7b1d5e42770
handboy <handersonmedeiros@gmail.com>
2019-09-29 23:12:24 -0300

    fix: id type

--- src/main/java/br/com/report/repository/ProblemRepository.java
+++ src/main/java/br/com/report/repository/ProblemRepository.java
@@ -3,5 +3,5 @@ package br.com.report.repository;
 import br.com.report.models.Problem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProblemRepository extends JpaRepository<Problem, String> {
+public interface ProblemRepository extends JpaRepository<Problem, Long> {
 }
