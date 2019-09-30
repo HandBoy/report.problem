cf5d40afadf369a28244df08218ac935d43205be
handboy <handersonmedeiros@gmail.com>
2019-09-29 23:16:21 -0300

    feat: get problem by id

--- src/main/java/br/com/report/resources/ProblemResources.java
+++ src/main/java/br/com/report/resources/ProblemResources.java
@@ -1,34 +1,48 @@
 package br.com.report.resources;
 
+import br.com.report.errors.ApiError;
 import br.com.report.models.Problem;
 import br.com.report.repository.ProblemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Date;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/problem")
 public class ProblemResources {
 
     @Autowired
-    private ProblemRepository er;
+    private ProblemRepository problemRepository;
 
-    @GetMapping(produces="application/json")
+    @GetMapping(produces = "application/json")
     public @ResponseBody
-    Iterable<Problem> listProblems(){
-        Iterable<Problem> problems = er.findAll();
+    Iterable<Problem> listProblems() {
+        Iterable<Problem> problems = problemRepository.findAll();
         return problems;
     }
 
+    @GetMapping(path = "/{problemId}")
+    public ResponseEntity<?> getProblem(@PathVariable(value = "problemId") long problemId) {
+        Optional<Problem> problem = problemRepository.findById(problemId);
+        if (problem.isPresent())
+            return new ResponseEntity<>(problem.get(), HttpStatus.OK);
+
+        return new ResponseEntity<>(new ApiError(404, "Problem not found", new Date()), HttpStatus.NOT_FOUND);
+    }
+
     @PostMapping()
-    public Problem addProblem(@RequestBody @Valid Problem problem){
-        return er.save(problem);
+    public Problem addProblem(@RequestBody @Valid Problem problem) {
+        return problemRepository.save(problem);
     }
 
     @DeleteMapping()
-    public Problem delProblem(@RequestBody Problem problem){
-        er.delete(problem);
+    public Problem delProblem(@RequestBody Problem problem) {
+        problemRepository.delete(problem);
         return problem;
     }
 
